77d730cc13d791dfac34c4730ff0462809b4ba36
kunal14 <kunal@healthcloudai.com>
2020-09-27 15:00:03 +0530

    added components -- 01

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -6,8 +6,10 @@ import Chat from "./components/Chat";
 function App() {
     return (
         <div className="app">
-            <SideBar/>
-            <Chat/>
+            <div className="app__body">
+                <SideBar/>
+                <Chat/>
+            </div>
         </div>
     );
 }
