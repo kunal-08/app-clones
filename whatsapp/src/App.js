9c35aad6a9dd7b1e188f70201ebb314f01a242b1
kunal14 <kunal@healthcloudai.com>
2020-09-27 14:41:59 +0530

    added first whatsapp clone

---
+++ whatsapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import SideBar from "./components/SideBar";
+import Chat from "./components/Chat";
+
+function App() {
+    return (
+        <div className="app">
+            <div className="">
+                <SideBar/>
+                <Chat/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ whatsapp/src/components/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Chat() {
+    return (
+        <div>
+            <h1>I am Chat Comp</h1>
+        </div>
+    );
+}
+
+export default Chat;
